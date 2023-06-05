384e7ad55d93928312cb84a46f55f68db7952bd1
Emanuele Roppo <ema.roppo@gmail.com>
2023-06-05 03:04:13 +0200

    added opt matching

--- montecarlo.py
+++ montecarlo.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tqdm import tqdm
+from hungarian_algorithm import hungarian_algorithm
 
 
 n_nodes=30
@@ -67,7 +68,71 @@ def greedy_algorithm(init_prob_matrix, budget, k, max_steps):
     
 
 
-print(greedy_algorithm(graph_probabilities, 5, 1000, 100))
+def get_reward(node_class, product_class):
+    return 100-np.random.normal(means[node_class-1, product_class], stds[node_class-1, product_class])
+
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+
+# con la versione dell'assistente si impantanava, fatto questo con chatgpt per ora, cerco di riparare l'altra
+def hungarian_algorithm(matrix):
+    m = matrix.copy()
+    n_rows, n_cols = m.shape
+    max_val = np.max(m)
+
+    if n_rows > n_cols:
+        m = np.pad(m, ((0, 0), (0, n_rows - n_cols)), mode='constant', constant_values=max_val)
+    elif n_cols > n_rows:
+        m = np.pad(m, ((0, n_cols - n_rows), (0, 0)), mode='constant', constant_values=max_val)
+
+    assigned_rows, assigned_cols = linear_sum_assignment(m)
+
+    assignment = np.zeros_like(m, dtype=int)
+    assignment[assigned_rows, assigned_cols] = 1
+
+    return assignment[:n_rows, :n_cols] * matrix, assignment[:n_rows, :n_cols]
+
+
+def exp_optimum(graph_probabilities, opt_seeds, n_exp):
+
+    rewards_per_exp=np.zeros(n_exp)
+
+    for i in tqdm(range(n_exp)):
+        active_nodes=simulate_episode(graph_probabilities, opt_seeds, 100)[1]*node_classes
+        #remove all zeros from array (nodes that were not activated)
+        active_nodes=active_nodes[active_nodes!=0]
+        #print(active_nodes)
+        #compute rewards
+        hun_matrix_dim=max(len(active_nodes), 9)
+        rewards=np.zeros((hun_matrix_dim, hun_matrix_dim))
+
+        for j in range(hun_matrix_dim):
+            for l in range(hun_matrix_dim):
+                if j<len(active_nodes) and l<9:
+                    rewards[j,l]=get_reward(int(active_nodes[j]), l//3)
+                else:
+                    rewards[j,l]=0
+
+        optimum= hungarian_algorithm(rewards)[0]
+        optimum= 100-optimum #convert to reward
+        #set all 100s to 0s
+        optimum[optimum==100]=0
+        #print(optimum)
+        rewards_per_exp[i]=np.sum(optimum)
+        #print('Optimum reward: ', np.sum(optimum))
+        #print('-------------------')
+    return np.mean(rewards_per_exp), np.std(rewards_per_exp)
+
+
+node_classes=np.random.randint(1,4, graph_probabilities.shape[0])
+#parameters for gaussian distribution
+means=np.random.uniform(10, 20, (3,3))
+stds=np.random.uniform(1, 3, (3,3))
+
+opt_seeds=greedy_algorithm(graph_probabilities, 3, 1000, 100)
+print(exp_optimum(graph_probabilities, opt_seeds, 1000)) 
+
+
 
 
 
