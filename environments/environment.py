5cbf104a1c9a1fc5af553047108ddf8b3710fb51
Emanuele Roppo <ema.roppo@gmail.com>
2023-06-09 17:15:29 +0200

    added environment for matching

--- environments/environment.py
+++ environments/environment.py
@@ -7,3 +7,18 @@ class Environment:
     def round(self, pulled_arm):
         reward = np.random.binomial(1, self.probabilities[pulled_arm])       
         return reward
+    
+class MatchingEnvironment(Environment):
+    def __init__(self, reward_matrix):
+        self.reward_matrix = reward_matrix
+        self.n_arms = reward_matrix.size
+        self.t = 0
+
+    def round(self, pulled_arm):
+        customer_indices, product_indices = pulled_arm
+        reward_functions = self.reward_matrix[customer_indices, product_indices]
+        # Apply each function or retrieve number to get rewards
+        reward = np.array([func() if callable(func) else func for func in reward_functions])
+
+
+        return reward
