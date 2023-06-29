052d4f8317818c88d6f5ef30f511caac16d4c7a7
Emanuele Roppo <ema.roppo@gmail.com>
2023-06-29 16:55:48 +0200

    fixed req 1 (still have to do ts)

--- 1_learning_for_social_influence.py
+++ 1_learning_for_social_influence.py
@@ -3,8 +3,8 @@ import numpy as np
 
 from tqdm import tqdm
 
-from environments.environment import Environment
-from learners.ucb_learner import UCBLearner
+from environments.environment import SocialEnvironment
+from learners.ucb_learner import UCBLearner, MAUCBLearner
 from learners.ts_learner import TSLearner
 #from clairvoyant import clairvoyant
 
@@ -26,45 +26,18 @@ n_episodes=20000
 
 
 #retrieve edge activation probabilities for each node
-envs=[]
-ucb_bandits=[]
+env=SocialEnvironment(graph_probabilities)
+ucb_bandit=MAUCBLearner(n_arms=30, n_seeds=3)
 ts_bandits=[]
+rewards=[]
 
-for i in graph_probabilities:
-    #drop 0s / non existing edges
-    p=i[i!=0]
-    #initialize environment for each node
-    env=Environment(p)
-    envs.append(env)
-    #initialize bandit for each node
-    ucb_bandit=UCBLearner(len(p))
-    ucb_bandits.append(ucb_bandit)
-    ts_bandit=TSLearner(len(p))
-    ts_bandits.append(ts_bandit)
+for i in tqdm(range(15000)):
+    reward=env.round(ucb_bandit.pull_arm())/3
+    rewards.append(reward)
+    ucb_bandit.update(ucb_bandit.pull_arm(), reward)
 
+    if i%1000==0: print(sum(rewards)/i)
+    
+print(sum(rewards[-1000:])/1000)
+print(ucb_bandit.empirical_means)
 
-for i in tqdm(range(n_episodes)):
-    for env, ucb_bandit, ts_bandit in zip(envs, ucb_bandits, ts_bandits):
-        
-        #pull arm
-        ucb_pulled_arm=ucb_bandit.pull_arm()
-        ts_pulled_arm=ts_bandit.pull_arm()
-        #retrieve reward
-        ucb_reward=env.round(ucb_pulled_arm)
-        ts_reward=env.round(ts_pulled_arm)
-        
-        #update bandit
-        ucb_bandit.update(ucb_pulled_arm, ucb_reward)
-        ts_bandit.update(ts_pulled_arm, ts_reward)
-
-
-#print estimated probabilities
-
-for env, ucb_bandit, ts_bandit in zip(envs, ucb_bandits, ts_bandits):
-    print(ucb_bandit.empirical_means)
-
-    print(env.probabilities)
-    #calculate probs from beta parameters
-    ts_probs=ts_bandit.beta_parameters[:,0]/(ts_bandit.beta_parameters[:,0]+ts_bandit.beta_parameters[:,1])
-    print(ts_probs)
-    print('------------------')
--- learners/ucb_learner.py
+++ learners/ucb_learner.py
@@ -27,6 +27,33 @@ class UCBLearner(Learner):
         
         self.update_observations(pulled_arm, reward)
 
+class MAUCBLearner(UCBLearner):
+    def __init__(self, n_arms, n_seeds):
+        super().__init__(n_arms)
+        self.n_seeds = n_seeds
+        self.n_pulls = np.zeros(n_arms)  # count the number of times each arm has been pulled
+    
+    def pull_arm(self):
+        upper_confidence_bound = self.empirical_means + self.confidence
+        pulled_arms = []
+        while len(pulled_arms) < self.n_seeds:
+            pulled_arm = np.random.choice(np.where(upper_confidence_bound == upper_confidence_bound.max())[0])
+            upper_confidence_bound[pulled_arm] = -np.inf
+            pulled_arms.append(pulled_arm)
+            
+        return pulled_arms
+
+    def update(self, pulled_arms, reward):
+        self.t += 1
+        for pulled_arm in pulled_arms:
+            self.n_pulls[pulled_arm] += 1
+            self.empirical_means[pulled_arm] = (self.empirical_means[pulled_arm] * (self.n_pulls[pulled_arm] - 1) + reward) / self.n_pulls[pulled_arm]
+            n_samples = max(1, self.n_pulls[pulled_arm])
+            self.confidence[pulled_arm] = np.sqrt(2 * np.log(self.t) / n_samples) if n_samples > 0 else np.inf
+
+            self.update_observations(pulled_arm, reward)
+
+
 class UCBMatching(UCBLearner):
     def __init__(self, n_arms, n_rows, n_cols):
         super().__init__(n_arms)
