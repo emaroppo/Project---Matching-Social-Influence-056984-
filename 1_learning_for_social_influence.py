e442a70b9b838db639b0d2f9ff789ba60c12ab1e
Emanuele Roppo <ema.roppo@gmail.com>
2023-07-01 15:28:58 +0200

    revised step 1 adding an alternative approach and tweaking functions to give outputs useful for project requirement

--- 1_learning_for_social_influence.py
+++ 1_learning_for_social_influence.py
@@ -3,9 +3,10 @@ import numpy as np
 
 from tqdm import tqdm
 
-from environments.environment import Environment
-from learners.ucb_learner import UCBLearner
+from environments.environment import Environment, SocialEnvironment
+from learners.ucb_learner import UCBLearner, MAUCBLearner
 from learners.ts_learner import TSLearner
+from utils.influence import greedy_algorithm
 #from clairvoyant import clairvoyant
 
 
@@ -24,47 +25,79 @@ reward_parameters=(means, stds)
 
 n_episodes=20000
 
+'''
+1. estimate activation probabilities implicitly by using bandits 
+to find set of seeds maximising reward'''
 
-#retrieve edge activation probabilities for each node
-envs=[]
-ucb_bandits=[]
-ts_bandits=[]
+def step_1(graph_probabilities, n_nodes, n_episodes):
+    env=SocialEnvironment(graph_probabilities)
+    ucb_bandit=MAUCBLearner(n_nodes, n_seeds=3)
+    rewards=[]
 
-for i in graph_probabilities:
-    #drop 0s / non existing edges
-    p=i[i!=0]
-    #initialize environment for each node
-    env=Environment(p)
-    envs.append(env)
-    #initialize bandit for each node
-    ucb_bandit=UCBLearner(len(p))
-    ucb_bandits.append(ucb_bandit)
-    ts_bandit=TSLearner(len(p))
-    ts_bandits.append(ts_bandit)
+    for i in tqdm(range(n_episodes)):
+        reward=env.round(ucb_bandit.pull_arm())/3
+        rewards.append(reward)
+        ucb_bandit.update(ucb_bandit.pull_arm(), reward)
 
-
-for i in tqdm(range(n_episodes)):
-    for env, ucb_bandit, ts_bandit in zip(envs, ucb_bandits, ts_bandits):
+        if i%1000==0: print(sum(rewards)/i)
         
-        #pull arm
-        ucb_pulled_arm=ucb_bandit.pull_arm()
-        ts_pulled_arm=ts_bandit.pull_arm()
-        #retrieve reward
-        ucb_reward=env.round(ucb_pulled_arm)
-        ts_reward=env.round(ts_pulled_arm)
-        
-        #update bandit
-        ucb_bandit.update(ucb_pulled_arm, ucb_reward)
-        ts_bandit.update(ts_pulled_arm, ts_reward)
+    return ucb_bandit, rewards
 
+'''
+1. estimate activation probabilities explicitly by using bandits
+2. use estimated probabilities to compute optimal seeds with greedy algo + montecarlo
+'''
 
-#print estimated probabilities
+#need to modify to keep track of reward for each episode 
+#(to plot regret etc.)
 
-for env, ucb_bandit, ts_bandit in zip(envs, ucb_bandits, ts_bandits):
-    print(ucb_bandit.empirical_means)
+def step_1v2(graph_probabilities, n_nodes, n_episodes):
+    
+    # retrieve edge activation probabilities for each node
+    envs = []
+    ucb_bandits = []
+    ts_bandits = []
 
-    print(env.probabilities)
-    #calculate probs from beta parameters
-    ts_probs=ts_bandit.beta_parameters[:,0]/(ts_bandit.beta_parameters[:,0]+ts_bandit.beta_parameters[:,1])
-    print(ts_probs)
-    print('------------------')
+    for p in graph_probabilities:
+        # drop 0s / non-existing edges
+        non_zero_indices = np.where(p != 0)[0]
+        # initialize environment for each node
+        env = Environment(p[non_zero_indices])
+        envs.append(env)
+        # initialize bandit for each node
+        ucb_bandit = UCBLearner(len(non_zero_indices))
+        ucb_bandits.append(ucb_bandit)
+        ts_bandit = TSLearner(len(non_zero_indices))
+        ts_bandits.append(ts_bandit)
+
+    for env, ucb_bandit, ts_bandit in tqdm(zip(envs, ucb_bandits, ts_bandits), total=len(envs), desc='Estimating Activation Probabilities...'):
+        for _ in range(n_episodes):
+            # pull arm
+            ucb_pulled_arm = ucb_bandit.pull_arm()
+            ts_pulled_arm = ts_bandit.pull_arm()
+            # retrieve reward
+            ucb_reward = env.round(ucb_pulled_arm)
+            ts_reward = env.round(ts_pulled_arm)
+
+            # update bandit
+            ucb_bandit.update(ucb_pulled_arm, ucb_reward)
+            ts_bandit.update(ts_pulled_arm, ts_reward)
+    
+    ucb_graph_probabilities = np.zeros(graph_probabilities.shape)
+    ts_graph_probabilities = np.zeros(graph_probabilities.shape)
+
+    # change all the non-0 terms of graph probabilities assigning respective bandit estimates
+    for i, p in enumerate(graph_probabilities):
+        non_zero_indices = np.where(p != 0)[0]
+        ucb_graph_probabilities[i][non_zero_indices] = ucb_bandits[i].empirical_means
+        ts_graph_probabilities[i][non_zero_indices] = ts_bandits[i].beta_parameters[:, 0] / np.sum(ts_bandits[i].beta_parameters, axis=1)
+
+    best_seeds_ucb=greedy_algorithm(ucb_graph_probabilities, 3, 1000, 100)
+    best_seeds_ts=greedy_algorithm(ts_graph_probabilities, 3, 1000, 100)
+
+    social_env=SocialEnvironment(graph_probabilities)
+
+    ts_rewards=[social_env.round(best_seeds_ts) for i in range(n_episodes)]
+    ucb_rewards=[social_env.round(best_seeds_ucb) for i in range(n_episodes)]
+
+    return sum(ts_rewards)/len(ts_rewards), sum(ucb_rewards)/len(ucb_rewards)
--- environments/environment.py
+++ environments/environment.py
@@ -7,7 +7,49 @@ class Environment:
     def round(self, pulled_arm):
         reward = np.random.binomial(1, self.probabilities[pulled_arm])       
         return reward
+
     
+class SocialEnvironment(Environment):
+    def __init__(self, probabilities):
+        super().__init__(probabilities)
+    
+    def simulate_episode(self, seeds:list, max_steps=100):
+        prob_matrix=self.probabilities.copy()
+        n_nodes=prob_matrix.shape[0]
+        
+        #set up seeds
+        active_nodes=np.zeros(n_nodes)
+        
+        for seed in seeds:
+            active_nodes[seed]=1
+        
+        history=np.array([active_nodes])
+
+        newly_active_nodes=active_nodes
+
+        t=0
+        
+        while (t<max_steps and np.sum(newly_active_nodes)>0):
+            #retrieve probability of edge activations
+            p = (prob_matrix.T*active_nodes).T
+            activated_edges=p>np.random.rand(p.shape[0], p.shape[1])
+            #remove activated edges
+            prob_matrix=prob_matrix*((p!=0)==activated_edges)
+            #update active nodes
+            newly_active_nodes=(np.sum(activated_edges, axis=0)>0)*(1-active_nodes)
+            #print(newly_active_nodes)
+            active_nodes=np.array(active_nodes+newly_active_nodes)
+            #print(active_nodes)
+            history=np.concatenate((history, [newly_active_nodes]), axis=0)
+            t+=1
+        return history, active_nodes
+    
+    def round(self, pulled_arms):
+        seeds= pulled_arms
+        history, active_nodes=self.simulate_episode(seeds)
+        reward = np.sum(active_nodes)
+        return reward
+
 class MatchingEnvironment(Environment):
     def __init__(self, reward_matrix):
         self.reward_matrix = reward_matrix
