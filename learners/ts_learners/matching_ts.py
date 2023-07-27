5e8d44c3fbc7e59b62d9a16061844f3a1cc011a8
ubuntu <ema.roppo@gmail.com>
2023-07-27 14:42:50 +0200

    updated learners to store rewards correctly

--- learners/ts_learners/matching_ts.py
+++ learners/ts_learners/matching_ts.py
@@ -2,44 +2,69 @@ from learners.ts_learners.ts_learner import TSLearner
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
+
 class TSMatching(TSLearner):
-    def __init__(self, n_arms, n_customer_classes, n_product_classes, n_products_per_class=3):
+    def __init__(
+        self, n_arms, n_customer_classes, n_product_classes, n_products_per_class=3
+    ):
         super().__init__(n_arms)
         self.n_customer_classes = n_customer_classes
         self.n_product_classes = n_product_classes
-        self.mu = np.zeros((n_customer_classes+1, n_product_classes+1))  # include the phantom class
-        self.n_samples = np.zeros((n_customer_classes+1, n_product_classes+1), dtype=int)
-        self.rewards_per_arm = {} #temporary fix
+        self.mu = np.zeros(
+            (n_customer_classes + 1, n_product_classes + 1)
+        )  # include the phantom class
+        self.n_samples = np.zeros(
+            (n_customer_classes + 1, n_product_classes + 1), dtype=int
+        )
+        self.rewards_per_arm = {}  # temporary fix
         self.n_products_per_class = n_products_per_class
-        
+
     def pull_arms(self, customer_classes):
         theta_sample = np.random.normal(self.mu, 1)
 
         # Extended theta_sample matrix to include phantom class
-        extended_theta_sample = np.zeros((self.n_customer_classes + 1, self.n_product_classes + 1))
-        extended_theta_sample[:self.n_customer_classes+1, :self.n_product_classes+1] = theta_sample
+        extended_theta_sample = np.zeros(
+            (self.n_customer_classes + 1, self.n_product_classes + 1)
+        )
+        extended_theta_sample[
+            : self.n_customer_classes + 1, : self.n_product_classes + 1
+        ] = theta_sample
 
         # Repeat entries in available_theta for each product
-        available_theta = np.repeat(extended_theta_sample[customer_classes, :], self.n_products_per_class, axis=1)
+        available_theta = np.repeat(
+            extended_theta_sample[customer_classes, :],
+            self.n_products_per_class,
+            axis=1,
+        )
         row_ind, col_ind = linear_sum_assignment(-available_theta)
-        best_arms_global = [(customer_classes[row] if row < len(customer_classes) else self.n_customer_classes, col//3)
-                            for row, col in zip(row_ind, col_ind)]
-        
+        best_arms_global = [
+            (
+                customer_classes[row]
+                if row < len(customer_classes)
+                else self.n_customer_classes,
+                col // 3,
+            )
+            for row, col in zip(row_ind, col_ind)
+        ]
 
         return best_arms_global
-    
 
     def update(self, pulled_arms, rewards):
+        self.collected_rewards = np.append(
+            self.collected_rewards, np.array(rewards).sum()
+        )
         for pulled_arm, reward in zip(pulled_arms, rewards):
             pulled_arm_customer, pulled_arm_product = pulled_arm  # unpacking tuple
             self.t += 1
             self.n_samples[pulled_arm_customer, pulled_arm_product] += 1
-            self.mu[pulled_arm_customer, pulled_arm_product] = (self.mu[pulled_arm_customer, pulled_arm_product] * (self.n_samples[pulled_arm_customer, pulled_arm_product] - 1) + reward) / self.n_samples[pulled_arm_customer, pulled_arm_product]
-            
+            self.mu[pulled_arm_customer, pulled_arm_product] = (
+                self.mu[pulled_arm_customer, pulled_arm_product]
+                * (self.n_samples[pulled_arm_customer, pulled_arm_product] - 1)
+                + reward
+            ) / self.n_samples[pulled_arm_customer, pulled_arm_product]
+
             # Update observations
             # If `self.rewards_per_arm` is a dictionary with tuple keys
             if pulled_arm not in self.rewards_per_arm:
                 self.rewards_per_arm[pulled_arm] = []
             self.rewards_per_arm[pulled_arm].append(reward)
-
-
--- learners/ucb_learners/matching_ucb.py
+++ learners/ucb_learners/matching_ucb.py
@@ -2,16 +2,21 @@ from learners.ucb_learners.ucb_learner import UCBLearner
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
+
 class UCBMatching(UCBLearner):
-    def __init__(self, n_arms, n_customer_classes, n_product_classes, n_products_per_class):
+    def __init__(
+        self, n_arms, n_customer_classes, n_product_classes, n_products_per_class
+    ):
         super().__init__(n_arms)
         self.n_customer_classes = n_customer_classes
         self.n_products_per_class = n_products_per_class
         self.n_product_classes = n_product_classes
         self.empirical_means = np.zeros((n_customer_classes + 1, n_product_classes + 1))
-        self.n_pulls = np.zeros((n_customer_classes + 1, n_product_classes + 1), dtype=int)
+        self.n_pulls = np.zeros(
+            (n_customer_classes + 1, n_product_classes + 1), dtype=int
+        )
         self.confidence = np.zeros((n_customer_classes + 1, n_product_classes + 1))
-        self.rewards_per_arm = {} #temporary fix
+        self.rewards_per_arm = {}  # temporary fix
 
     def pull_arms(self, customer_classes):
         upper_conf = self.empirical_means + self.confidence
@@ -20,17 +25,29 @@ class UCBMatching(UCBLearner):
         available_upper_conf = extended_upper_conf[customer_classes, :]
         row_ind, col_ind = linear_sum_assignment(-available_upper_conf)
 
-        best_arms_global = [(customer_classes[row] if row < len(customer_classes) else self.n_customer_classes, col//3) 
-                            for row, col in zip(row_ind, col_ind)]
+        best_arms_global = [
+            (
+                customer_classes[row]
+                if row < len(customer_classes)
+                else self.n_customer_classes,
+                col // 3,
+            )
+            for row, col in zip(row_ind, col_ind)
+        ]
         return best_arms_global
 
     def update(self, pulled_arms, rewards):
+        self.collected_rewards = np.append(self.collected_rewards, rewards.sum())
         for pulled_arm, reward in zip(pulled_arms, rewards):
             pulled_arm_customer, pulled_arm_product = pulled_arm  # unpacking tuple
             self.t += 1
-            
+
             self.n_pulls[pulled_arm_customer, pulled_arm_product] += 1
-            self.empirical_means[pulled_arm_customer, pulled_arm_product] = (self.empirical_means[pulled_arm_customer, pulled_arm_product] * (self.n_pulls[pulled_arm_customer, pulled_arm_product] - 1) + reward) / self.n_pulls[pulled_arm_customer, pulled_arm_product]            
+            self.empirical_means[pulled_arm_customer, pulled_arm_product] = (
+                self.empirical_means[pulled_arm_customer, pulled_arm_product]
+                * (self.n_pulls[pulled_arm_customer, pulled_arm_product] - 1)
+                + reward
+            ) / self.n_pulls[pulled_arm_customer, pulled_arm_product]
             # Update observations
             # If `self.rewards_per_arm` is a dictionary with tuple keys
             if pulled_arm not in self.rewards_per_arm:
