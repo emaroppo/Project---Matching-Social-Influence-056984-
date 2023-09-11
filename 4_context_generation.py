76d9f28445520131046a7d3d3859dc24aefc1731
Emanuele Roppo <ema.roppo@gmail.com>
2023-09-11 18:19:16 +0200

    step 4 mostly done

--- 4_context_generation.py
+++ 4_context_generation.py
@@ -1,29 +1,135 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from learners.ts_learners.matching_ts import TSMatching4
+from learners.ts_learners.ts_learner import TSProbLearner
 from environments.joint_environment import JointEnvironment
+from environments.matching_environment import MatchingEnvironment2
+from context import ContextGenerationAlgorithm
+from tqdm import tqdm
+from metrics import compute_metrics, plot_metrics
+from data_generator import generate_graph, generate_reward_parameters
 
-node_features = np.random.binomial(1, 0.3, (30, 10))
+# init reward matrix, graph probabilities
+n_nodes = 30
+edge_rate = 0.2
+
+n_seeds = 3
+n_customer_classes = 3
+n_product_classes = 3
+products_per_class = 3
+n_exp = 365
+
+reward_means, reward_std_dev = generate_reward_parameters(
+    n_customer_classes, n_product_classes
+)
+graph_probabilities, graph_structure = generate_graph(n_nodes, edge_rate)
+# link node ids to customer classes
+node_classes = np.random.randint(0, n_customer_classes, n_nodes)
 
 
-class ContextGeneration:
-    def __init__(self) -> None:
-        customer_features = np.random.binomial(1, 0.3, (30, 2))  # list of customers
-        assignments = np.random.randint(0, 3, 30)  # list of customers/products
-        rewards = np.random.normal(
-            0, 1, (30, 3)
-        )  # list of rewards for each customer/product
-        # compute expected reward for each feature/product pair
-        expected_rewards = np.zeros((2, 3))
-        for i in range(2):
-            for j in range(3):
-                expected_rewards[i][j] = np.mean(rewards[assignments == j, i])
-        # compute lower confidence bound for each feature/product pair
-        lower_confidence_bound = np.zeros((2, 3))
-        for i in range(2):
-            for j in range(3):
-                lower_confidence_bound[i][j] = expected_rewards[i][j] - np.sqrt(
-                    np.log(1) / 1
-                )
-        # compute lower confidence bound of probability occurence for each feature
-        lower_confidence_bound_prob = np.zeros(2)
-        for i in range(2):
-            lower_confidence_bound_prob[i] = np.mean(lower_confidence_bound[i])
+node_features = np.random.binomial(1, 0.3, (30, 3))
+
+
+# initialise bandit
+ts_bandit = TSProbLearner(n_nodes, n_seeds, graph_structure=graph_structure)
+ts_matching = TSMatching4(n_product_classes*1, 1, n_product_classes, products_per_class)
+
+# initialise environment
+joint_env = JointEnvironment(
+    graph_probabilities, (reward_means, reward_std_dev), node_classes
+)
+matching_env = MatchingEnvironment2((reward_means, reward_std_dev))
+
+expected_social_rewards = []
+# opt_social_reward = joint_env.social_environment.opt(n_seeds)[1]
+expected_matching_rewards = []
+optimal_matching_rewards = []
+
+context_generator = ContextGenerationAlgorithm(2, [0,1,2])
+dataset = []
+for m in tqdm(range(n_exp)[5:]):
+
+    
+
+    # pull arm
+    
+    ts_pulled_arm = ts_bandit.pull_arm()
+
+    # retrieve episode
+    ts_social_reward, active_nodes = joint_env.social_environment.round(
+        ts_pulled_arm, joint=True
+    )
+
+    ts_bandit.update(ts_social_reward)
+    expected_social_reward = [
+        joint_env.social_environment.round(ts_pulled_arm, joint=True)[1].sum()
+        for _ in range(1000)
+    ]
+
+    expected_social_rewards.append(np.mean(expected_social_reward))
+
+    if m>13:
+        rules= context_generator.context_structures[-1].create_rules()
+        mapping=context_generator.context_structures[-1].create_mapping(node_features, rules)
+    else:
+        mapping = [0]*n_nodes
+    
+    active_nodes_id = np.array(active_nodes)
+    # convert to list of integer indices corresponding to the position of activated nodes in the reward matrix
+    active_nodes_id = np.where(active_nodes == 1)[-1]
+    active_estimated_classes = [mapping[i] for i in active_nodes_id]
+
+    # convert to list of classes
+    active_real_classes = [node_classes[i] for i in active_nodes_id]
+
+    # perform matching assuming customer classes are known but distributions unknown
+    ts_prop_match = ts_matching.pull_arms(active_nodes_id, active_estimated_classes)
+    # retrieve reward
+    ts_matching_reward = matching_env.round(ts_prop_match)
+
+    dataset_entry_features=[node_features[i[0]] for i in ts_matching_reward]
+    
+    dataset_entry_rewards= [np.zeros(4) for i in ts_matching_reward]
+    for i,j in enumerate(dataset_entry_rewards):
+        j[ts_prop_match[i][2]]= ts_matching_reward[i][1]
+
+    #merge features and rewards
+    #features are the first 3 columns, rewards the last 3
+    dataset_entry = np.concatenate((dataset_entry_features, dataset_entry_rewards), axis=1)
+
+    dataset.append(dataset_entry)
+
+    expected_matching_reward = [
+        matching_env.round(ts_prop_match) for _ in range(1000)
+    ]
+    expected_matching_reward = [expected_matching_reward[i][1] for i in range(len(expected_matching_reward))]
+
+    expected_matching_reward = np.mean(expected_matching_reward)
+
+    optimal_matching_reward = joint_env.matching_environment.opt(
+        active_real_classes, [0, 1, 2] * 3
+    )
+    optimal_matching_rewards.append(optimal_matching_reward)
+
+    expected_matching_rewards.append(np.mean(expected_matching_reward))
+    # update bandit
+
+    ts_matching.update(ts_prop_match, ts_matching_reward)
+
+    if (m+1) % 14 == 0:
+        context_generator.update(dataset)
+        ts_matching.resize_arms(context_generator.context_structures[-1])
+
+# plot expected social rewards
+metrics = compute_metrics(
+    np.array(expected_social_rewards),
+    np.array([joint_env.social_environment.opt(n_seeds)[0]] * n_exp),
+)
+plot_metrics(*metrics, model_name="TS", env_name="Joint (Social) TS")
+
+# plot expected matching rewards
+expected_matching_rewards = np.array(expected_matching_rewards) / np.array(
+    optimal_matching_rewards
+)
+metrics = compute_metrics(expected_matching_rewards, opt_rewards=np.ones(n_exp))
+plot_metrics(*metrics, model_name="TS", env_name="Joint (Matching) TS")
--- context.py
+++ context.py
@@ -1,9 +1,14 @@
 import numpy as np
-from environments.matching_environment import MatchingEnvironment
+from scipy.optimize import linear_sum_assignment  # Assuming you are using scipy's method
+import copy
+
+
 
 
 class TreeNode:
-    def __init__(self, children=list(), parent=None) -> None:
+    def __init__(self, children=None, parent=None) -> None:
+        if children is None:
+            children = []
         self.children = children
         self.parent = parent
 
@@ -11,18 +16,39 @@ class TreeNode:
         self.children.append(child)
         child.parent = self
 
-
 class Context(TreeNode):
     def __init__(self, split, parent=None, dataset=None, n_product_classes=3):
-        super().__init__(parent)
-        self.split = split  # ordered list of features, values (0 or 1)
-        self.dataset_features = dataset[:, :-n_product_classes]
-        self.dataset_rewards = dataset[:, -n_product_classes:]
+        super().__init__(parent=parent)
+        self.split = split if split is not None else []  # ordered list of features, values (0 or 1)
+        self.dataset = dataset
+        self.n_product_classes = n_product_classes
+        self.dataset_features = dataset[:, :-n_product_classes] if dataset is not None else None
+        self.dataset_rewards = dataset[:, -n_product_classes:] if dataset is not None else None
 
-        self.expected_reward = self.expected_prob = len(self.dataset) / len(
-            self.parent.dataset
-        )
-        self.expected_prob = np.mean(self.dataset_rewards)
+        self.expected_reward = 0
+        self.expected_prob = 0
+        if self.parent and self.parent.dataset is not None and len(self.parent.dataset) > 0:
+            self.expected_prob = len(self.dataset) / len(self.parent.dataset)
+            self.expected_reward = np.mean(self.dataset_rewards)
+
+    def update_features_and_rewards(self):
+        self.dataset_features = self.dataset[:, :-self.n_product_classes] if self.dataset is not None else None
+        self.dataset_rewards = self.dataset[:, -self.n_product_classes:] if self.dataset is not None else None
+
+
+
+    def propagate_dataset_down(self):
+        if not self.children:  # Base case: if it's a leaf node, no further action needed
+            return
+
+        # If it's not a leaf node, split its dataset and propagate down to its children
+        for child in self.children:
+            feature, value = child.split[-1]  # the last split added corresponds to this child
+            child.dataset = self.dataset[self.dataset[:, feature] == value]
+            print( self.dataset[self.dataset[:, feature] == value])
+            child.dataset_features = child.dataset[:, :-self.product_classes] if child.dataset is not None else None
+            child.dataset_rewards = child.dataset[:, -self.product_classes:] if child.dataset is not None else None
+            child.propagate_dataset_down()  # Recursively do the same for this child
 
     def split_context(self, feature, value):
         # split dataset into children
@@ -34,16 +60,22 @@ class Context(TreeNode):
         dataset_complement = self.dataset[self.dataset[:, feature] != value]
         # create children
         child = Context(child_split, self, dataset)
-        child_complement = Context(child_split, self, dataset_complement)
+        #complement split
+        complement_split = self.split.copy()
+        complement_split.append([feature, 1-value])
+
+        child_complement = Context(complement_split, self, dataset_complement)
 
         return child, child_complement
 
 
 class ContextStructure:
     def __init__(self, episodes, n_features, product_classes) -> None:
-        self.episodes = episodes
-        dataset = np.concatenate(episodes, axis=0)
-        self.root = Context(None, dataset=dataset)
+        if episodes is None:
+            self.episodes = list()
+        else:
+            self.episodes = episodes
+        self.root = Context(None, dataset=episodes)
         self.n_features = n_features
         self.product_classes = product_classes
         self.leaves = [self.root]
@@ -61,6 +93,7 @@ class ContextStructure:
                     self.features_mapping[feature][value] = list()
                 self.features_mapping[feature][value].append(i)
 
+    @staticmethod
     def create_mapping(data, rules):
         mapping = {}
 
@@ -71,6 +104,9 @@ class ContextStructure:
                     break
 
         return mapping
+    
+    def copy(self):
+        return copy.deepcopy(self)
 
     def create_reward_matrix(self, leaf_nodes=None):
         if leaf_nodes is None:
@@ -79,60 +115,65 @@ class ContextStructure:
         n_product_classes = len(self.product_classes)
         reward_matrix = np.zeros((n_customer_classes, n_product_classes))
         for i, j in enumerate(leaf_nodes):
+            print(j.dataset_rewards)
+            j.update_features_and_rewards()
             reward_matrix[i] = np.mean(j.dataset_rewards, axis=0)
 
         return reward_matrix
 
-    def split_context(self, context, feature, value):
+    def split_context(self, context_index, feature, value):
+        #copy current context structure
+        context_structure = self.copy()
+        # split argument context into children
+        context=context_structure.leaves[context_index]
         child, child_complement = context.split_context(feature, value)
+        context_structure.leaves.remove(child.parent)
+        context_structure.leaves.append(child)
+        context_structure.leaves.append(child_complement)
 
-        return child, child_complement
+        return context_structure
 
-    def confirm_split(self, feature, value):
-        child, child_complement = self.split_context(feature, value)
-        self.children.append(child)
-        self.children.append(child_complement)
-        self.leaves.remove(child.parent)
-        self.leaves.append(child)
-        self.leaves.append(child_complement)
-
-    def test_split(self, context, feature, value, cumulative_reward=None):
-        child, child_complement = self.split_context(context, feature, value)
-        if cumulative_reward is None:
-            # calculate cumulative reward
-            pass
-        # calculate expected reward of proposed split
-        # create reward matrix of proposed split
-        reward_matrix = self.create_reward_matrix([child, child_complement])
+    def create_rules(self):
         rules = []
-        for i in self.leaves + [child, child_complement]:
+        for i in self.leaves:
             rule = dict()
             for j in i.split:
                 rule[j[0]] = j[1]
             rules.append(rule)
-        world_rep = MatchingEnvironment(reward_matrix)
 
-        # calculate expected reward of proposed split
-        class_occurences = np.zeros(len(self.leaves) + 2)
-        awards = np.zeros(len(self.leaves) + 2)
+        return rules     
 
-        for episode in self.episodes:
-            episode_customers, episode_rewards = (
-                episode[:, : self.product_classes],
-                episode[:, -self.product_classes :],
-            )
-            customer_classes_mapping = self.create_mapping(episode, rules)
-            customer_classes = [
-                customer_classes_mapping[i] for i in range(len(episode_customers))
-            ]
-            world_rep.opt(
-                customer_classes=customer_classes, product_classes=[0, 1, 2] * 3
-            )
-            class_occurences += np.array(
-                [customer_classes.count(i) for i in range(len(self.leaves) + 2)]
-            )
+    def test_context_structure(self):
+        reward_matrix = self.create_reward_matrix()
+        rules = self.create_rules()
 
-        return world_rep.opt(), cumulative_reward
+        
+        product_classes=[0,1,2]*3
+        
+        episode_cum_rew=0
+        for item in self.episodes:
+            for episode in item:
+                episode_customers, episode_rewards = (
+                    episode[:, : len(self.product_classes)],
+                    episode[:, -len(self.product_classes) :],
+                )
+                customer_classes_mapping = self.create_mapping(episode, rules)
+                customer_classes = [
+                    customer_classes_mapping[i] for i in range(len(episode_customers))
+                ]
+
+                #from list of customer classes, product classes and reward matrix, create reward matrix for episode, compute optimal assignment
+                episode_table = np.zeros((len(customer_classes), len(product_classes
+                                                                    )))
+
+                for i, j in enumerate(customer_classes):
+                    for k, l in enumerate(product_classes):
+                        episode_table[i, k] = reward_matrix[j, l]
+
+                episode_assignment = linear_sum_assignment(-episode_table)
+                episode_cum_rew += episode_table[episode_assignment].sum()
+
+        return episode_cum_rew
 
 
 class ContextGenerationAlgorithm:
@@ -143,25 +184,39 @@ class ContextGenerationAlgorithm:
         self.dataset = []
 
     def update_dataset(self, episodes):
-        self.dataset.append(episodes)
+        self.dataset.extend(episodes)
+        self.context_structures[-1].root.dataset = np.concatenate(self.dataset, axis=0)
+        print(self.context_structures[-1].episodes.append(episodes))
+        print(self.context_structures[-1].root.dataset.shape)
+        self.context_structures[-1].root.propagate_dataset_down()  # Propagate the dataset down the tree
 
     def update_context_structures(self, context_structure):
         self.context_structures.append(context_structure)
 
     def update(self, episodes):
         self.update_dataset(episodes)
-        # retrieve cumulative reward of current context structure
-        cumulative_reward = None
-        new_expected_reward = None
-        while new_expected_reward > cumulative_reward:
-            for i in leaf_nodes:
-                exp_reward = context_structure.test_split(
-                    i, feature, value, cumulative_reward
-                )
-                if exp_reward > cumulative_reward:
-                    i.confirm_split(feature, value)
-                    break
-        
-    
-    def generate_context_structure(self):
 
+        improvement_found = True  # To keep track if we found an improvement
+        while improvement_found:
+            improvement_found = False  # Initially assume no improvement
+            # Start with the latest context structure
+            current_structure = self.context_structures[-1]
+            current_reward = current_structure.test_context_structure()  # Initial reward
+            for leaf_index in range(len(current_structure.leaves)):
+                for feature in range(self.n_features):
+                    for value in [0, 1]:  # Assuming binary split
+                        # Attempt a split and create a new context structure
+                        proposed_structure = current_structure.split_context(leaf_index, feature, value)
+                        proposed_reward = proposed_structure.test_context_structure()
+                        print(proposed_reward)
+                        
+                        # Check if the proposed structure is better
+                        if proposed_reward > current_reward:
+                            # Replace with the new structure and set flag to start over
+                            self.update_context_structures(proposed_structure)
+                            improvement_found = True
+                            break  # Break out of the innermost loop
+                if improvement_found:
+                    break  # If found an improvement, break out of this loop to start over
+            
+        return self.context_structures[-1]  # Return the best structure
