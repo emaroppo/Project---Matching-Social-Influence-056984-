d6828bab2c99cff8ae3138f0c6f7eb31ec8c9a63
ubuntu <ema.roppo@gmail.com>
2023-09-12 18:16:56 +0200

    added classes for non stationary activations

--- learners/ucb_learners/ucb_learner.py
+++ learners/ucb_learners/ucb_learner.py
@@ -125,3 +125,60 @@ class UCBProbLearner:
 
         self.confidence[self.graph_structure == 0] = 0
         self.collected_rewards.append(np.sum(activated_edges))
+
+
+class UCBProbLearner2:
+    def __init__(self, n_nodes, n_seeds, graph_structure=None):
+        self.n_nodes = n_nodes
+        self.n_seeds = n_seeds
+        self.empirical_means = np.zeros((n_nodes, n_nodes))
+        self.confidence = np.full((n_nodes, n_nodes), 1000)
+        self.graph_structure = graph_structure
+        if graph_structure is not None:
+            mask = graph_structure == 0
+            self.empirical_means[mask] = 0
+            self.confidence[mask] = 0
+
+        self.edge_rewards = np.zeros((n_nodes, n_nodes))
+        self.n_pulls = np.zeros((n_nodes, n_nodes))
+        self.t = 0
+        self.collected_rewards = []
+
+        # Store all episodes
+        self.episodes = []
+
+    def pull_arm(self):
+        sqrt_factor = np.sqrt(max(1, (365 - self.t)) / 365)
+        upper_confidence_bound = (
+            self.empirical_means + sqrt_factor * self.confidence
+        ) / (1 + sqrt_factor * self.confidence)
+        np.clip(upper_confidence_bound, None, 10, out=upper_confidence_bound)
+
+        world_representation = SocialEnvironment(upper_confidence_bound)
+        seeds = world_representation.opt_arm(self.n_seeds)
+        return seeds
+
+    def update_observations(self, episode):
+        self.episodes.append(episode)
+        self.t += 1
+        for step in episode:
+            active_nodes, newly_active_nodes, activated_edges = step
+            susceptible_edges = np.outer(newly_active_nodes, 1 - active_nodes)
+
+            self.n_pulls += susceptible_edges
+            self.edge_rewards += activated_edges
+
+    def update(self, episode):
+        self.update_observations(episode)
+
+        # Recompute empirical means and confidence based on all episodes
+        self.empirical_means[self.n_pulls != 0] = (
+            self.edge_rewards[self.n_pulls != 0] / self.n_pulls[self.n_pulls != 0]
+        )
+        self.confidence[self.n_pulls != 0] = np.sqrt(
+            2 * np.log(1 + np.sum(self.n_pulls)) / self.n_pulls[self.n_pulls != 0]
+        )
+
+        if self.graph_structure is not None:
+            self.confidence[self.graph_structure == 0] = 0
+        self.collected_rewards.append(np.sum(self.edge_rewards))
