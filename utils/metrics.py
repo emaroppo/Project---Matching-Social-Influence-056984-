fixed std computation

utils/metrics.py
@@ -7,13 +7,22 @@ from typing import List, Dict
 def compute_metrics(model, env):
     metrics = dict()
     metrics['instantaneous_reward'] = model.expected_rewards
+    
     #calculate instantaneous regret expected value (var[i][0]) and std (var[i][1])
     expected_inst_regret = env.optimal_rewards[:,0] - model.expected_rewards[:,0]
-    std_inst_regret = env.optimal_rewards[:,1] + model.expected_rewards[:,1]
+    std_inst_regret = np.sqrt(env.optimal_rewards[:,1]**2 + model.expected_rewards[:,1]**2)
     metrics['instantaneous_regret'] = np.array([expected_inst_regret, std_inst_regret]).T #why do i have to Transpose?
+    
     #calculate cumulative reward expected value (var[i][0]) and std (var[i][1]) (cumsum of expected rewards and std of rewards)
-    metrics['cumulative_reward'] = np.cumsum(env.optimal_rewards, axis=0)
-    metrics['cumulative_regret'] = np.cumsum(metrics['instantaneous_regret'], axis=0)
+    expected_cumulative_reward = np.cumsum(model.expected_rewards[:,0])
+    std_cumulative_reward = np.sqrt(np.cumsum(model.expected_rewards[:,1]**2))
+    metrics['cumulative_reward'] = np.array([expected_cumulative_reward, std_cumulative_reward]).T
+    
+    #calculate cumulative regret expected value (var[i][0]) and std (var[i][1]) (cumsum of expected regret and std of regret)
+    expected_cumulative_regret = np.cumsum(expected_inst_regret)
+    std_cumulative_regret = np.sqrt(np.cumsum(std_inst_regret**2))
+    metrics['cumulative_regret'] = np.array([expected_cumulative_regret, std_cumulative_regret]).T
+    
 
     return metrics
 