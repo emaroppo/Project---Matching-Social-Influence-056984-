10fac87ec2b0c59d04369066ea6eb5dd582ee1f9
Emanuele Roppo <ema.roppo@gmail.com>
2023-12-12 00:11:57 +0100

    tidied plots

--- utils/metrics.py
+++ utils/metrics.py
@@ -4,25 +4,34 @@ import os
 import time
 from typing import List, Dict
 
+
 def compute_metrics(model, env):
     metrics = dict()
-    metrics['instantaneous_reward'] = model.expected_rewards
-    
-    #calculate instantaneous regret expected value (var[i][0]) and std (var[i][1])
-    expected_inst_regret = env.optimal_rewards[:,0] - model.expected_rewards[:,0]
-    std_inst_regret = np.sqrt(env.optimal_rewards[:,1]**2 + model.expected_rewards[:,1]**2)
-    metrics['instantaneous_regret'] = np.array([expected_inst_regret, std_inst_regret]).T #why do i have to Transpose?
-    
-    #calculate cumulative reward expected value (var[i][0]) and std (var[i][1]) (cumsum of expected rewards and std of rewards)
-    expected_cumulative_reward = np.cumsum(model.expected_rewards[:,0])
-    std_cumulative_reward = np.sqrt(np.cumsum(model.expected_rewards[:,1]**2))
-    metrics['cumulative_reward'] = np.array([expected_cumulative_reward, std_cumulative_reward]).T
-    
-    #calculate cumulative regret expected value (var[i][0]) and std (var[i][1]) (cumsum of expected regret and std of regret)
+    metrics["model_name"] = model.__class__.__name__
+    metrics["instantaneous_reward"] = model.expected_rewards
+
+    # calculate instantaneous regret expected value (var[i][0]) and std (var[i][1])
+    expected_inst_regret = env.optimal_rewards[:, 0] - model.expected_rewards[:, 0]
+    std_inst_regret = np.sqrt(
+        env.optimal_rewards[:, 1] ** 2 + model.expected_rewards[:, 1] ** 2
+    )
+    metrics["instantaneous_regret"] = np.array(
+        [expected_inst_regret, std_inst_regret]
+    ).T  # why do i have to Transpose?
+
+    # calculate cumulative reward expected value (var[i][0]) and std (var[i][1]) (cumsum of expected rewards and std of rewards)
+    expected_cumulative_reward = np.cumsum(model.expected_rewards[:, 0])
+    std_cumulative_reward = np.sqrt(np.cumsum(model.expected_rewards[:, 1] ** 2))
+    metrics["cumulative_reward"] = np.array(
+        [expected_cumulative_reward, std_cumulative_reward]
+    ).T
+
+    # calculate cumulative regret expected value (var[i][0]) and std (var[i][1]) (cumsum of expected regret and std of regret)
     expected_cumulative_regret = np.cumsum(expected_inst_regret)
     std_cumulative_regret = np.sqrt(np.cumsum(std_inst_regret**2))
-    metrics['cumulative_regret'] = np.array([expected_cumulative_regret, std_cumulative_regret]).T
-    
+    metrics["cumulative_regret"] = np.array(
+        [expected_cumulative_regret, std_cumulative_regret]
+    ).T
 
     return metrics
 
@@ -39,7 +48,10 @@ def plot_metrics(metrics_list: List[Dict], env_name=None):
 
     # Plot each metric
     for metric_name in metric_names:
+        if metric_name == "model_name":
+            continue
         plt.figure()
+        
         for model_metrics in metrics_list:
             if metric_name in model_metrics:
                 # Extract the expected value and standard deviation
@@ -48,15 +60,27 @@ def plot_metrics(metrics_list: List[Dict], env_name=None):
                 timesteps = range(len(expected_values))
 
                 # Plot the expected values
-                plt.plot(timesteps, expected_values, label=model_metrics.get('model_name', 'Unknown Model'))
+                plt.plot(
+                    timesteps,
+                    expected_values,
+                    label=model_metrics.get("model_name", "Unknown Model"),
+                )
 
                 # Add a colored band for the standard deviation
-                plt.fill_between(timesteps, expected_values - std_devs, expected_values + std_devs, alpha=0.2)
+                plt.fill_between(
+                    timesteps,
+                    expected_values - std_devs,
+                    expected_values + std_devs,
+                    alpha=0.2,
+                )
 
-        plt.title(f'{metric_name} over Time' + ('' if env_name is None else f' in {env_name}'))
-        plt.xlabel('Time Step')
-        plt.ylabel(metric_name)
+        plt.title(
+            f'{ " ".join(metric_name.split("_")).title()} over Time'
+            + ("" if env_name is None else f" in {env_name}")
+        )
+        plt.xlabel("Time Step")
+        plt.ylabel(" ".join(metric_name.split("_")).title())
         plt.legend()
-        plt.savefig(f'{folder_name}/{metric_name}.png')
+        plt.savefig(f"{folder_name}/{metric_name}.png")
         plt.show()
         plt.close()
